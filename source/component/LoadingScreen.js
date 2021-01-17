4f11701fd99ec8cd0e8159fb1187214cd728d7d7
Allan <luminussora@gmail.com>
2021-01-17 00:25:34 -0300

    loading page

--- source/component/LoadingScreen.js
+++ source/component/LoadingScreen.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React, {useEffect} from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 
-export default class LoginScreen extends Component {
-  render() {
-    return (
-      <View style={styles.background}>
-        <View style={styles.screen}>
-          <View style={{ marginBottom: '32%' }}>
-            <View style={styles.logo}></View>
-            <Image></Image>
-            <Text style={styles.titleText}>Recruta Humanizado</Text>
-          </View>
+export default function LoginScreen({ navigation }) {
 
-          <View style={{marginBottom: '45%'}}>
-            <View style={styles.logo}></View>
-            <Text style={styles.commomText}>Carregando</Text>
-          </View>
+  useEffect(() => {
+    setTimeout(function () {
+      navigation.navigate('PerfilFirstScreen');
+    }.bind(this), 3000);
+  });
 
-          <Text style={styles.bottomText}>Sabia que até hoje mais de 50 mil brasileiros conseguiram a sua primeira experiência profissional usando Recruta Humanizado?</Text>
-        </View>
+  return (
+    <View style={styles.background}>
+      <View style={styles.screen}>
+        <Image style={styles.logo}></Image>
+        <Text style={styles.titleText}>Recruta Humanizado</Text>
+        <ActivityIndicator size={'large'} animating={true} color={'#4460F1'} />
+        <Text style={styles.commomText}>Carregando</Text>
+        <Text style={styles.bottomText}>Sabia que até hoje mais de 50 mil brasileiros conseguiram a sua primeira experiência profissional usando Recruta Humanizado?</Text>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -46,10 +45,12 @@ const styles = StyleSheet.create({
     color: '#204559',
     fontSize: 22,
     fontWeight: 'bold',
+    marginBottom: '32%'
   },
   commomText: {
     color: '#204559',
     fontSize: 20,
+    marginBottom: '45%'
   },
   bottomText: {
     color: '#204559',
